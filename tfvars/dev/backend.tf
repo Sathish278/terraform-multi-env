a3b94e9a8fab9d1ffebb57360020db12a41e0476
Ambati Sathish Kumar Reddy <ambatisathishkumarreddy@gmail.com>
2024-05-15 18:26:54 +0530

    adding tfvars and workspaces

--- tfvars/dev/backend.tf
+++ tfvars/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "Sathish-s3-demo-dev" # change this
-    key            = "sathish-dev/terraform.tfstate"
+    key            = "multi-env-tfvars"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock"
--- tfvars/prod/backend.tf
+++ tfvars/prod/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "sathish-s3-demo-prod" # change this
-    key            = "sathish-prod/terraform.tfstate"
+    key            = "multi-env-tfvars"
     region         = "us-east-1"
     encrypt        = true
     dynamodb_table = "terraform-lock-prod"
